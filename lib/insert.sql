b08a88686e1b0a9d919c300abe435b0041b254cd
Sam Chappel <m.samchappel@gmail.com>
2023-03-07 13:14:16 -0800

    lab completed

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES
+  ('Mr. Chocolate', 5, 'M', 'brown', 'grumpy', 1),
+  ('Rowdy', 3, 'M', 'black', 'energetic', 1),
+  ('Tabitha', 7, 'F', 'gray', 'calm', 1),
+  ('Sergeant Brown', 9, 'M', 'brown', 'strict', 0),
+  ('Melissa', 2, 'F', 'white', 'shy', 1),
+  ('Grinch', 6, 'M', 'green', 'sneaky', 0),
+  ('Wendy', 4, 'F', 'brown', 'friendly', 1),
+  (NULL, 10, 'M', 'brown', 'grumpy', 1);
